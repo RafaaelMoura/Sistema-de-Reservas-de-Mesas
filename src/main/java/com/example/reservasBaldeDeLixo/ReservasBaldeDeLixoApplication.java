dadfc49a1bc1147a7333a02e4dc22e9e3657db9e
Rafael Moura <rafamour03@gmail.com>
2026-05-10 16:39:19 -0300

    feat: criação do sistema, configuração do docker compose e application.yaml

---
+++ src/main/java/com/example/reservasBaldeDeLixo/ReservasBaldeDeLixoApplication.java
@@ -0,0 +1,13 @@
+package com.example.reservasBaldeDeLixo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasBaldeDeLixoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasBaldeDeLixoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reservasBaldeDeLixo/ReservasBaldeDeLixoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reservasBaldeDeLixo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasBaldeDeLixoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
