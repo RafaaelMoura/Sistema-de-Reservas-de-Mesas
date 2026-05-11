8025d71c3d8b1579f50d0e8aba39f7e5803dd4da
Rafael Moura <rafamour03@gmail.com>
2026-05-11 16:42:47 -0300

    feat: implementou BeansConfig e metodo de criar dos clientes

--- src/main/java/com/example/reservasBaldeDeLixo/config/BeansConfig.java
+++ src/main/java/com/example/reservasBaldeDeLixo/config/BeansConfig.java
@@ -1,5 +1,17 @@
 package com.example.reservasBaldeDeLixo.config;
 
+import com.example.reservasBaldeDeLixo.application.port.input.CriarCliente;
+import com.example.reservasBaldeDeLixo.application.port.output.ClientesGateway;
+import com.example.reservasBaldeDeLixo.application.usecase.CriaClienteUsecaseImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class BeansConfig {
+
+    @Bean
+    public CriarCliente criarCliente(ClientesGateway clientesGateway) {
+        return new CriaClienteUsecaseImpl(clientesGateway);
+    }
 
 }
