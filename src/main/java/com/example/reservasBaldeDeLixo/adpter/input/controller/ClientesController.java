08e11fc6e1ffb3149139c4bc5278dd23b23b3290
Rafael Moura <rafamour03@gmail.com>
2026-05-15 19:06:17 -0300

    feat: implementou controller de metodos POST e GET

--- src/main/java/com/example/reservasBaldeDeLixo/adpter/input/controller/ClientesController.java
+++ src/main/java/com/example/reservasBaldeDeLixo/adpter/input/controller/ClientesController.java
@@ -3,26 +3,40 @@ package com.example.reservasBaldeDeLixo.adpter.input.controller;
 import com.example.reservasBaldeDeLixo.adpter.input.dtos.ClientesDto;
 import com.example.reservasBaldeDeLixo.adpter.input.dtos.ClientesDtoMapper;
 import com.example.reservasBaldeDeLixo.application.domain.model.Clientes;
-import com.example.reservasBaldeDeLixo.application.port.input.CriarCliente;
+import com.example.reservasBaldeDeLixo.application.port.input.CriarClienteUsecase;
+import com.example.reservasBaldeDeLixo.application.port.input.ListarClientesInput;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("V1/baldedelixo")
-@RequiredArgsConstructor
+@RequestMapping("v1/baldedelixo")
 public class ClientesController {
 
-    private final CriarCliente criarCliente;
+    private final CriarClienteUsecase criarClienteUsecase;
     private final ClientesDtoMapper mapper;
+    private final ListarClientesInput listarClientesInput;
 
+    public ClientesController(CriarClienteUsecase criarClienteUsecase, ClientesDtoMapper mapper, ListarClientesInput listarClientesInput) {
+        this.criarClienteUsecase = criarClienteUsecase;
+        this.mapper = mapper;
+        this.listarClientesInput = listarClientesInput;
+    }
 
-    @PostMapping("criar")
+    @PostMapping("/criarcliente")
     public ClientesDto criar(@RequestBody ClientesDto clientesDto) {
-        Clientes criarClientes = criarCliente.execute(mapper.toDoamin(clientesDto));
+        Clientes criarClientes = criarClienteUsecase.execute(mapper.toDoamin(clientesDto));
         return mapper.toDto(criarClientes);
+    }
+
+    @GetMapping("/listadeclientes")
+    public List<ClientesDto> listar() {
+        return listarClientesInput.execute()
+                .stream()
+                .map(mapper::toDto)
+                .collect(Collectors.toList());
     }
 
 }
