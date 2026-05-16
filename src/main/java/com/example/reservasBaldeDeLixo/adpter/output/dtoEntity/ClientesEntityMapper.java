653294a64519d71b12c04902c7df959fe8d90ddc
Rafael Moura <rafamour03@gmail.com>
2026-05-16 18:04:55 -0300

    adicionou mapper da entidade de clientes

--- src/main/java/com/example/reservasBaldeDeLixo/adpter/output/dtoEntity/ClientesEntityMapper.java
+++ src/main/java/com/example/reservasBaldeDeLixo/adpter/output/dtoEntity/ClientesEntityMapper.java
@@ -1,6 +1,27 @@
 package com.example.reservasBaldeDeLixo.adpter.output.dtoEntity;
 
+import com.example.reservasBaldeDeLixo.adpter.output.model.ClientesEntity;
+import com.example.reservasBaldeDeLixo.application.domain.model.Clientes;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ClientesEntityMapper {
 
+    public ClientesEntity toEntity(Clientes clientes) {
+        return new ClientesEntity(
+                clientes.id(),
+                clientes.nome(),
+                clientes.telefone(),
+                clientes.email()
+        );
+    }
 
+    public Clientes toDomain(ClientesEntity clientesEntity) {
+        return new Clientes(
+                clientesEntity.getId(),
+                clientesEntity.getNome(),
+                clientesEntity.getTelefone(),
+                clientesEntity.getEmail()
+        );
+    }
 }
