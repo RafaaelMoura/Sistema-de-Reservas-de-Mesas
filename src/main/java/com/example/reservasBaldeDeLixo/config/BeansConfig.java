1f695c2c4fd3b5cc534f990fa456f7436194a51a
Rafael Moura <rafamour03@gmail.com>
2026-05-16 18:07:52 -0300

    implementou bens config de criar clientes e listar clientes

--- src/main/java/com/example/reservasBaldeDeLixo/config/BeansConfig.java
+++ src/main/java/com/example/reservasBaldeDeLixo/config/BeansConfig.java
@@ -1,8 +1,10 @@
 package com.example.reservasBaldeDeLixo.config;
 
-import com.example.reservasBaldeDeLixo.application.port.input.CriarCliente;
+import com.example.reservasBaldeDeLixo.application.port.input.CriarClienteUsecase;
+import com.example.reservasBaldeDeLixo.application.port.input.ListarClientesInput;
 import com.example.reservasBaldeDeLixo.application.port.output.ClientesGateway;
 import com.example.reservasBaldeDeLixo.application.usecase.CriaClienteUsecaseImpl;
+import com.example.reservasBaldeDeLixo.application.usecase.ListarClienteImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,8 +12,12 @@ import org.springframework.context.annotation.Configuration;
 public class BeansConfig {
 
     @Bean
-    public CriarCliente criarCliente(ClientesGateway clientesGateway) {
+    public CriarClienteUsecase criarCliente(ClientesGateway clientesGateway) {
         return new CriaClienteUsecaseImpl(clientesGateway);
+    }
+    @Bean
+    public ListarClientesInput listarTodosClientes(ClientesGateway clientesGateway) {
+        return new ListarClienteImpl(clientesGateway);
     }
 
 }
