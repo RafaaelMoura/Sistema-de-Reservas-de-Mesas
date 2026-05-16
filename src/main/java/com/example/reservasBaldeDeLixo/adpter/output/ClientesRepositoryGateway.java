67d02aeb76b2003c79e5b1b020ae42c33fc3fe58
Rafael Moura <rafamour03@gmail.com>
2026-05-16 18:01:43 -0300

    feat: adicionou getways implementacoes e interfaces

--- src/main/java/com/example/reservasBaldeDeLixo/adpter/output/ClientesRepositoryGateway.java
+++ src/main/java/com/example/reservasBaldeDeLixo/adpter/output/ClientesRepositoryGateway.java
@@ -1,4 +1,42 @@
 package com.example.reservasBaldeDeLixo.adpter.output;
 
-public class ClientesRepositoryGateway {
+import com.example.reservasBaldeDeLixo.adpter.output.dtoEntity.ClientesEntityMapper;
+import com.example.reservasBaldeDeLixo.adpter.output.model.ClientesEntity;
+import com.example.reservasBaldeDeLixo.adpter.output.model.ClientesRepository;
+import com.example.reservasBaldeDeLixo.application.domain.model.Clientes;
+import com.example.reservasBaldeDeLixo.application.port.output.ClientesGateway;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class ClientesRepositoryGateway implements ClientesGateway {
+
+    /*
+    Como esse metodo instacia diretamente o Jpa e ele quem faz a interação com o banco de dado, ele precisa de um
+    mapper proprio do jpa
+    */
+
+    private final ClientesRepository clientesRepository;
+    private final ClientesEntityMapper entityMapper;
+
+
+    @Override
+    public Clientes criar(Clientes clientes) {
+        ClientesEntity entity = entityMapper.toEntity(clientes);
+        ClientesEntity novoCliente = clientesRepository.save(entity);
+        return entityMapper.toDomain(novoCliente);
+
+    }
+
+    @Override
+    public List<Clientes> buscarClientes() {
+        return clientesRepository.findAll()
+                .stream()
+                .map(entityMapper::toDomain).toList();
+    }
+
+
 }
--- src/main/java/com/example/reservasBaldeDeLixo/application/port/output/ClientesGateway.java
+++ src/main/java/com/example/reservasBaldeDeLixo/application/port/output/ClientesGateway.java
@@ -2,7 +2,12 @@ package com.example.reservasBaldeDeLixo.application.port.output;
 
 import com.example.reservasBaldeDeLixo.application.domain.model.Clientes;
 
+import java.util.List;
+
 public interface ClientesGateway {
 
     Clientes criar(Clientes clientes);
+
+    List<Clientes> buscarClientes();
+
 }
